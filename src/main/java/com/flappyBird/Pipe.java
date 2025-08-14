da30f878208321826d993dd66e050b6f9c65ec8a
Borofad <vacheslavsevcenko35888@gmail.com>
2025-08-14 17:16:41 +0300

    Update Pipe.java

--- src/main/java/com/flappyBird/Pipe.java
+++ src/main/java/com/flappyBird/Pipe.java
@@ -9,7 +9,10 @@ public class Pipe {
     private Rectangle bottomPipe;
     private Group view;
     private double x;
-
+    private double topY;
+    private double bottomY;
+    private double topH;
+    private double bottomH;
     public Pipe(double width){
         topPipe = new Rectangle(width, 0, Color.GREEN);
         bottomPipe = new Rectangle(width, 0, Color.GREEN);
@@ -24,6 +27,13 @@ public class Pipe {
     public void updateView(){
         topPipe.setX(x);
         bottomPipe.setX(x);
+
+        topPipe.setY(0);
+        topPipe.setHeight(topH);
+
+        bottomPipe.setY(bottomY);
+        bottomPipe.setHeight(bottomH);
+
     }
 
     public Group getView() {
@@ -52,12 +62,30 @@ public class Pipe {
         double gapHeight = Math.random() * (gameAreaHeight / 3 - gameAreaHeight / 4) + gameAreaHeight / 4;
         double gapY = Math.random() * (gameAreaHeight - gapHeight);
 
-        topPipe.setY(0);
-        topPipe.setHeight(gapY);
+        topY = 0;
+        topH = gapY;
 
-        bottomPipe.setY(gapHeight + gapY);
-        bottomPipe.setHeight(gameAreaHeight - (gapHeight + gapY));
+        bottomY = gapHeight + gapY;
+        bottomH = gameAreaHeight - (gapHeight + gapY);
+    }
 
-        updateView();
+    public void setX(double x){
+        this.x = x;
+    }
+
+    public double getTopY() {
+        return topY;
+    }
+
+    public double getBottomY() {
+        return bottomY;
+    }
+
+    public double getTopH() {
+        return topH;
+    }
+
+    public double getBottomH() {
+        return bottomH;
     }
 }
